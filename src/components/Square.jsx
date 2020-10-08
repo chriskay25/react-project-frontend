a7fc38b8824b8ce0b29f84e55edb376a5a9d657f
Chris Kay <ckay84@protonmail.com>
2020-10-08 19:46:31 -0400

    Color and label squares

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
+
+  squareColor = () => {
+    switch (this.props.row % 2 === 0) {
+      case true && this.props.column % 2 !== 0:
+        return "lightcoral"
+      default:
+        return "cornsilk"
+    }
+  }
+
   render() {
     return (
-      <div>
-        {this.props.value}
+      <div className="square" style={{backgroundColor: this.squareColor()}}>
+        {this.props.row.toString() + "-" + this.props.column}
       </div>
     )
   }
