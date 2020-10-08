dcde3539aba781a579e5e59101a7b00f82e53023
Chris Kay <ckay84@protonmail.com>
2020-10-08 16:27:54 -0400

    Render row of Squares on Board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import Square from './Square';
 
 
 class Board extends Component {
 
+  renderSquare = (i) => {
+    return (
+      <Square value={i} />
+    )
+  }
   render() {
     return (
-      <div>
-        Board
+      <div className="board">
+        <table>
+          <tr>
+            <td>{this.renderSquare(1)}</td>
+            <td>{this.renderSquare(2)}</td>
+            <td>{this.renderSquare(3)}</td>
+            <td>{this.renderSquare(4)}</td>
+            <td>{this.renderSquare(5)}</td>
+            <td>{this.renderSquare(6)}</td>
+            <td>{this.renderSquare(7)}</td>
+            <td>{this.renderSquare(8)}</td>
+          </tr>
+        </table>
       </div>
     )
   }
