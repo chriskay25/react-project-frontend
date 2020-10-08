cb930594e16b192a3a38b68c6c0ee5b5de5ac948
Chris Kay <ckay84@protonmail.com>
2020-10-08 18:27:42 -0400

    Add fn to create rows, pass row & col as props to Square

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,25 +4,40 @@ import Square from './Square';
 
 class Board extends Component {
 
-  renderSquare = (i) => {
+  renderSquare = (x,y) => {
     return (
-      <Square value={i} />
+      <Square row={x} column={y} />
     )
   }
+
+  renderRow = (x) => {
+    return (
+      <tr row={x}>
+        <td>{this.renderSquare(x, 1)}</td>
+        <td>{this.renderSquare(x, 2)}</td>
+        <td>{this.renderSquare(x, 3)}</td>
+        <td>{this.renderSquare(x, 4)}</td>
+        <td>{this.renderSquare(x, 5)}</td>
+        <td>{this.renderSquare(x, 6)}</td>
+        <td>{this.renderSquare(x, 7)}</td>
+        <td>{this.renderSquare(x, 8)}</td>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <div className="board">
         <table>
-          <tr>
-            <td>{this.renderSquare(1)}</td>
-            <td>{this.renderSquare(2)}</td>
-            <td>{this.renderSquare(3)}</td>
-            <td>{this.renderSquare(4)}</td>
-            <td>{this.renderSquare(5)}</td>
-            <td>{this.renderSquare(6)}</td>
-            <td>{this.renderSquare(7)}</td>
-            <td>{this.renderSquare(8)}</td>
-          </tr>
+          {console.log(this)}
+          {this.renderRow("A")}
+          {this.renderRow("B")}
+          {this.renderRow("C")}
+          {this.renderRow("D")}
+          {this.renderRow("E")}
+          {this.renderRow("F")}
+          {this.renderRow("G")}
+          {this.renderRow("H")}
         </table>
       </div>
     )
