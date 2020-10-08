2929cb2215e01552303f52df7b99dee211020236
Chris Kay <ckay84@protonmail.com>
2020-10-08 15:38:37 -0400

    Create Board component, add to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Board from './components/Board'
 
 function App() {
   return (
     <div className="App">
       React Project
+      <Board />
     </div>
   );
 }
