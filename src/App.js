ba8c249d4f118149fbfd1878905cf46c2f99b272
Chris Kay <ckay84@protonmail.com>
2020-10-08 16:05:01 -0400

    Add title to App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Board from './components/Board'
 function App() {
   return (
     <div className="App">
-      React Project
+      <h1 class="title">React Project</h1>
       <Board />
     </div>
   );
