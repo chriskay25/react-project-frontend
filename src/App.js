da64834fd6cbcd62e69aad5c2552ab19e819e618
Chris Kay <ckay84@protonmail.com>
2020-10-13 12:31:41 -0400

    Import dependencies and add Router to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import './App.css';
-// import {connect} from 'react-redux'
-import Board from './components/Board'
+// import {connect} from 'react-redux';
+import Board from './components/Board';
+import UserForm from './components/UserForm';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="title">React Project</h1>
-      <Board />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Route exact path="/users/new" component={UserForm} />
+        <h1 className="title">React Project</h1>
+        <Board />
+      </div>
+    </Router>
   );
 }
 
