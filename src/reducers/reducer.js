faf3c4d107782afbaedcdd0eed1d9167c175a0cd
Chris Kay <ckay84@protonmail.com>
2020-10-14 16:26:15 -0400

    Add action type FETCH_USERS to reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,5 +1,7 @@
 export default function reducer(state = {users: []}, action) {
   switch (action.type) {
+    case 'FETCH_USERS':
+      return {users: action.payload}
     case 'ADD_USER':
       return {...state, users: [...state.users, action.payload]}
     default:
