6036151660fd641961af86c922abfa600ebb9547
Chris Kay <ckay84@protonmail.com>
2020-10-15 23:46:30 -0400

    Remove router and clean up App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import { 
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
 import './App.css';
-// import {connect} from 'react-redux';
 import Board from './components/Board';
-import UserForm from './components/UserForm';
 import NavBar from './components/NavBar';
+import UsersContainer from './containers/UsersContainer';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Route exact path="/users/new" component={UserForm} />
-        <h1 className="title">React Project</h1>
-        <Board />
-      </div>
-    </Router>
+    <div className="App">
+      <NavBar />
+      <h1 className="title">React Project</h1>
+      <UsersContainer />
+      <Board />
+    </div>
   );
 }
 
