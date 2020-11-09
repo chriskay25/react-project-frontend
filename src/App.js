a8b95ac7f76938a3c9efb447377fefd5d19e0997
Chris Kay <ckay84@protonmail.com>
2020-11-09 14:39:14 -0500

    Remove NavBar from App, inside UsersContainer now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
 import UsersContainer from './containers/UsersContainer';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <h1 className="title">React Project</h1>
+      <h1 style={{textAlign: 'center'}}>JUKE</h1>
       <UsersContainer />
     </div>
   );
