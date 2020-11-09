f4580e4962a8c5236196efe52fe540c5e702169d
Chris Kay <ckay84@protonmail.com>
2020-11-09 14:58:28 -0500

    Position logout bttn at top right of page

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logout = ({ handleLogout }) => {
   return (
-    <form className="UserLogout" onSubmit={handleLogout}>
+    <form className="UserLogout" onSubmit={handleLogout} style={{position: 'absolute', right: 10, top: 10}}>
       <input type="submit" value="Logout" />
     </form>
   )
