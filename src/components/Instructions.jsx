87b1e2a93d3d685e34d6c1dd64ea184e1fc00c34
Chris Kay <ckay84@protonmail.com>
2020-11-09 14:57:46 -0500

    Light edits to instructions

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -4,16 +4,16 @@ const Instructions = () => {
 
   const style = () => {
     return {
-      fontSize: 'medium',
+      fontSize: '.5em',
       textAlign: 'center'
     }
   }
 
   return (
     <div className="Instructions" style={style()}>
-      <p><strong>Instructions: </strong>This game is best played on a desktop.</p>
-      <p>Use the arrow keys to move the green circle. Avoid oncoming enemies and stay alive.</p> 
-      <p>The longer you stay alive, the higher your score. Good luck!</p>
+      <p><strong>Note: </strong>This game is best played on a desktop.</p>
+      <p>Use the arrow keys to move the green circle. Press the space bar to pause.</p> 
+      <p>Avoid oncoming enemies and stay alive. The longer you stay alive, the higher your score. Good luck!</p>
     </div>
   )
 
