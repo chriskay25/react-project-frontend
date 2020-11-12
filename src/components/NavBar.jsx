d8791096d71700814bd8c8aa93d2eb1587298d10
Chris Kay <ckay84@protonmail.com>
2020-11-11 23:08:06 -0500

    Replace regular header with imported image

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,21 +3,26 @@ import {Route, Link, Switch} from 'react-router-dom'
 import Logout from './Logout'
 import SignupForm from './SignupForm'
 import LoginForm from './LoginForm'
+import titleLogo from '../juke-title-5.png'
 
 const NavBar = ({ loggedIn, handleFormChange, handleLoginFormSubmit, handleLogout }) => {
 
   if (loggedIn) {
     return (
-      <div className="NavBar">
-        <h1 style={{textAlign: 'center', fontSize: '2.5rem'}}>JUKE</h1>
+      <div className='NavBar'>
+        <div className='AppTitle' style={{display: 'flex', padding: '1rem'}}>
+          <img src={titleLogo} className='titleImage' style={{maxHeight: '4rem'}} />
+        </div>
         <Logout handleLogout={handleLogout} />
       </div>
     )
   } else {
     return (
-      <div className="NavBar">
-        <h1 style={{textAlign: 'center', fontSize: '3rem'}}>JUKE</h1>
-        <p style={{fontSize: '1.5rem', paddingLeft: '1em', marginBottom: '2rem'}}>Please sign up or log in to play</p>
+      <div className='NavBar'>
+        <div className="AppTitle" style={{display: 'flex', padding: '2rem'}}>
+          <img src={titleLogo} className='titleImage' style={{maxHeight: '4rem'}} />
+        </div>
+        <p style={{fontSize: '1.5rem', paddingLeft: '1em', marginBottom: '2rem', color: 'white'}}>Please sign up or log in to play</p>
         <Link to='/users/new' style={{marginLeft: '2rem', padding: '.5rem 1rem'}}> Sign Up</Link>
         <Link to='/login' style={{padding: '.5rem 1rem'}}>Log In</Link>
 
