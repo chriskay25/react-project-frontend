fc78b7b8943b9746ff29463a4086510e77b8892c
Chris Kay <ckay84@protonmail.com>
2020-11-14 16:34:48 -0500

    Move css control to stylesheet for instructions and gamestats

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -2,17 +2,8 @@ import React from 'react'
 
 const Instructions = () => {
 
-  const style = () => {
-    return {
-      // fontSize: '.5em',
-      textAlign: 'center',
-      color: 'white',
-      padding: '1rem'
-    }
-  }
-
   return (
-    <div className="Instructions" style={style()}>
+    <div className="Instructions">
       <p><strong>Note: </strong>This game is best played on a desktop.</p>
       <p>Use the arrow keys to move the green circle. Press the space bar to pause.</p> 
       <p>Avoid oncoming enemies and stay alive. The longer you stay alive, the higher your score. Good luck!</p>
