23549bce32ae29c1a8b5185f2033dbbc31009d0d
Chris Kay <ckay84@protonmail.com>
2020-11-13 23:58:17 -0500

    Revamp NavBar to add functionality and move most css to stylesheet

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,37 +3,39 @@ import {Route, Link, Switch} from 'react-router-dom'
 import Logout from './Logout'
 import SignupForm from './SignupForm'
 import LoginForm from './LoginForm'
-import titleLogo from '../juke-title-25.png'
+import titleLogo from '../assets/juke-title-28.png'
 
 const NavBar = ({ loggedIn, handleFormChange, handleLoginFormSubmit, handleLogout }) => {
 
   if (loggedIn) {
     return (
-      <div className='NavBar'>
-        <div className='AppTitle' style={{display: 'flex', paddingTop: '1rem'}}>
-          <img src={titleLogo} className='titleImage' style={{maxHeight: '7rem', margin: '0 auto'}} />
+      <div className='NavBar LoggedIn'>
+        <div className="loggedInLogo">
+          <img src={titleLogo} className='logo' alt='logo' style={{margin: '0 auto'}} />
         </div>
         <Logout handleLogout={handleLogout} />
       </div>
     )
   } else {
     return (
-      <div className='NavBar'>
-        <div className="AppTitle" style={{display: 'flex', paddingTop: '2rem', minHeight: '15rem'}}>
-          <img src={titleLogo} className='titleImage' style={{maxHeight: '14rem', justifyContent: 'center', alignItems: 'center'}} />
+      <div className='NavBar LoggedOut'>
+        <div className="loggedOutLogo">
+          <img src={titleLogo} className='logo' alt='logo' />
         </div>
-        <p style={{fontSize: '1.5rem', padding: '1rem 0 1rem 1rem', marginBottom: '1rem', color: 'white'}}>Please sign up or log in to play</p>
-        <Link to='/users/new' style={{marginLeft: '2rem', padding: '.5rem 1rem'}}> Sign Up</Link>
-        <Link to='/login' style={{padding: '.5rem 1rem'}}>Log In</Link>
+        <div className='auth-section'>
+          <p>PLEASE SIGN UP OR LOG IN TO PLAY</p>
+          <Link to='/users/new' style={{marginLeft: '2rem'}}> Sign Up</Link>
+          <Link to='/login'>Log In</Link>
 
-        <Switch>
-          <Route exact path="/users/new">
-            <SignupForm handleFormChange={handleFormChange} />
-          </Route>
-          <Route exact path="/login">
-            <LoginForm handleFormChange={handleFormChange} handleLoginFormSubmit={handleLoginFormSubmit} />
-          </Route>
-        </Switch>
+          <Switch>
+            <Route exact path="/users/new">
+              <SignupForm handleFormChange={handleFormChange} />
+            </Route>
+            <Route exact path="/login">
+              <LoginForm handleFormChange={handleFormChange} handleLoginFormSubmit={handleLoginFormSubmit} />
+            </Route>
+          </Switch>
+        </div>
       </div>
     )
   }
