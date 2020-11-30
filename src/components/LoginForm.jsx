e1be8d5741fa3c7647a6abb4d9f0e79c4c5007d3
Chris Kay <ckay84@protonmail.com>
2020-11-30 17:17:53 -0500

    Make LoginForm a class comp that dispatches userLogin action on submit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { userLogin } from '../actions/userLogin';
 
+class LoginForm extends Component {
 
-const LoginForm = ({handleFormChange, handleLoginFormSubmit, username, password}) => {
-  return (
-    <div className="FormBox">
-      <h4 style={{padding: '1em'}}>LOGIN</h4>
-      <form className="InputForm" onSubmit={handleLoginFormSubmit}>
-        <label htmlFor="username">Username: </label>
-        <input type="text" placeholder="Username" name="username" value={username} onChange={handleFormChange} />
-        <br />
-        <label htmlFor="password">Password: </label>
-        <input type="password" placeholder="Password" name="password" value={password} onChange={handleFormChange} />
-        <br />
-        <input type="submit" value="Login"/>
-      </form>
-    </div>
-  )
+  state = {
+    username: '',
+    password: ''
   }
 
-export default LoginForm
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    // const { username, password } = this.state
+    this.props.userLogin(this.state)
+    this.setState({
+      username: '',
+      password: '',
+    })
+  }
+
+  render() {
+    const { username, password } = this.state
+    return (
+      <div className="FormBox" >
+        <h4 style={{padding: '1em'}}>LOGIN</h4>
+        <form className="InputForm" onSubmit={this.handleSubmit}>
+          <label htmlFor="username">Username: </label>
+          <input type="text" placeholder="Username" name="username" value={username} onChange={this.handleChange} />
+          <br />
+          <label htmlFor="password">Password: </label>
+          <input type="password" placeholder="Password" name="password" value={password} onChange={this.handleChange} />
+          <br />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect(null, { userLogin })(LoginForm)
