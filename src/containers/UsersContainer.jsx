94f3a4fa365686784276d23afc40d7b09e6ef27e
Chris Kay <ckay84@protonmail.com>
2020-11-30 17:29:40 -0500

    Connect UsersContainer to the store to get currentUser, delete handlers for LoginForm

--- src/containers/UsersContainer.jsx
+++ src/containers/UsersContainer.jsx
@@ -1,96 +1,19 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// eslint-disable-next-line no-unused-vars
-import User from '../components/User';
+import { connect } from 'react-redux';
 import GameContainer from './GameContainer';
 import NavBar from '../components/NavBar';
 import Instructions from '../components/Instructions';
-// import { fetchUsers } from '../actions/fetchUsers';
+import { userLogout } from '../actions/userLogout'
 
 class UsersContainer extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      currentUser: null,
-      loginForm: {
-        username: "",
-        password: ""
-      }
-    }
-  }
-
-  componentDidMount() {
-    const token = localStorage.getItem('token')
-    if (token) {
-      fetch('http://localhost:3000/api/v1/get_current_user', {
-        headers: {
-          'Authorization': token,
-        }
-      })
-        .then(resp => resp.json())
-        .then(user => {
-          console.log("Component Did Mount: ", user)
-          if (user.error) {
-            alert(user.error)
-          } else {
-            this.setState({
-              currentUser: user
-            }) 
-          }
-        })
-    }
-  }
-
-  handleLoginFormSubmit = event => {
-    event.preventDefault()
-    const userInfo = this.state.loginForm
-    const headers = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        user: userInfo
-      })
-    }
-    fetch("http://localhost:3000/api/v1/login", headers)
-      .then(resp => resp.json())
-      .then(userJSON => {
-        console.log("User JSON returned from login: ", userJSON)
-        if (userJSON.error) {  // in case there are server errors
-          alert("Invalid Credentials")
-        } else {
-          localStorage.setItem('token', userJSON.jwt)
-          this.setState({
-            currentUser: userJSON.user,
-          })
-        }
-      })
-      .catch(console.log)   // in case there are JS errors
-  }
-
-  handleFormChange = event => {
-    const { name, value } = event.target
-    this.setState({
-      loginForm: {
-        ...this.state.loginForm,
-        [name]: value
-      }
-    })
-  }
-
   handleLogout = event => {
     event.preventDefault()
-    localStorage.removeItem('token')
-    this.setState({
-      currentUser: null
-    })
+    this.props.userLogout()
   }
 
-
   render() {
-    const { currentUser } = this.state
+    const { currentUser } = this.props
     const win = window.visualViewport.width * .7
     const boardSize = win > 850 ? 850 : win
     const playerSize = boardSize / 30
@@ -99,17 +22,25 @@ class UsersContainer extends Component {
         <>
           <NavBar loggedIn={true} handleLogout={this.handleLogout} />
           <Instructions />
-          <GameContainer boardSize={boardSize} playerSize={playerSize} currentUser={currentUser} />
+          <GameContainer boardSize={boardSize} playerSize={playerSize} currentUser={this.props.currentUser} />
         </>
       )
     } else {
       return (
         <div>
-          <NavBar loggedIn={false} handleFormChange={this.handleFormChange} handleLoginFormSubmit={this.handleLoginFormSubmit} />
+          <NavBar loggedIn={false} />
         </div>
       )
     }
   }
 }
 
-export default UsersContainer
+const mapState = (state) => ({
+  currentUser: state.currentUser
+})
+
+
+export default connect(
+  mapState,
+  {userLogout}
+)(UsersContainer)
