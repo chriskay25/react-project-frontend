6c8502277fc11aefa12fbfa580b3b9ec70098e75
Chris Kay <ckay84@protonmail.com>
2020-12-01 16:51:19 -0500

    Add action to fetch current user if token is present

---
+++ src/actions/getCurrentUser.js
@@ -0,0 +1,20 @@
+export const getCurrentUser = () => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    return (dispatch) => {
+      fetch('http://localhost:3000/api/v1/get_current_user', {
+        headers: {
+          'Authorization': token,
+        }
+      })
+        .then(resp => resp.json())
+        .then(user => {
+          if (user.error) {
+            alert(user.error)
+          } else {
+            dispatch({type: 'GET_CURRENT_USER', payload: user})
+          }
+        })
+    }
+  }
+}
