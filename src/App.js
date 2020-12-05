94faedd083f4be649def4414180e3625d8e86d34
Chris Kay <ckay84@protonmail.com>
2020-12-05 00:00:57 -0500

    Move NavBar to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import UsersContainer from './containers/UsersContainer';
+import NavBar from './components/NavBar'
+import SignupForm from './components/SignupForm'
+import LoginForm from './components/LoginForm'
+import Logout from './components/Logout'
+import Logo from './components/Logo'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <UsersContainer />
+      <NavBar />
+      <Logo />
+      <Switch>
+        <Route exact path="/users/new" component={SignupForm} />
+        <Route exact path="/login" component={LoginForm} />
+      </Switch>
+      <Switch>
+      <Route exact path="/logout" component={Logout} />
+      </Switch>
+      {/* <UsersContainer /> */}
     </div>
   );
 }
