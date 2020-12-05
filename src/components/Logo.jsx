f31f4d9f7700962f433a33fc4d59a98daa856238
Chris Kay <ckay84@protonmail.com>
2020-12-05 00:01:33 -0500

    Make a logo comp

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import titleLogo from '../assets/juke-title-28.png'
+
+const Logo = () => {
+  return (
+    <div className='titleLogo'>
+      <img src={titleLogo} className='logo' alt='logo' />
+    </div>
+  )
+}
+
+export default Logo
