c4c92cbf3c5c218609aaf68b7a14e8adaf8db6de
Chris Kay <ckay84@protonmail.com>
2021-04-19 00:01:51 -0400

    Remove NavBar div

--- src/containers/UsersContainer.jsx
+++ src/containers/UsersContainer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import GameContainer from './GameContainer';
-import NavBar from '../components/NavBar';
 import Instructions from '../components/Instructions';
 import { userLogout } from '../actions/userLogout'
 import { getCurrentUser } from '../actions/getCurrentUser'
@@ -13,20 +12,12 @@ class UsersContainer extends Component {
     const win = window.visualViewport.width * .7
     const boardSize = win > 850 ? 850 : win
     const playerSize = boardSize / 30
-    if (currentUser) {
-      return (
-        <>
-          <Instructions />
-          <GameContainer boardSize={boardSize} playerSize={playerSize} currentUser={this.props.currentUser} />
-        </>
-      )
-    } else {
-      return (
-        <div>
-          {/* <NavBar loggedIn={false} /> */}
-        </div>
-      )
-    }
+    return (
+      <>
+        <Instructions />
+        <GameContainer boardSize={boardSize} playerSize={playerSize} currentUser={currentUser} />
+      </>
+    )
   }
 
   componentDidMount() {
