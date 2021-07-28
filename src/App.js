03d2c84c0b1ca591a01ca3b114a692521bcb3591
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-07-28 16:51:37 -0400

    Simplify App so it shows Home or Auth depending on login status, move Logout to Home

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import UsersContainer from './containers/UsersContainer';
 import { connect } from 'react-redux';
-import Logout from './components/Logout'
 import AuthForms from './components/AuthForms'
+import Home from './components/Home'
 import Logo from './components/Logo'
 import { getCurrentUser } from './actions/getCurrentUser'
-import { userLogout } from './actions/userLogout'
 
 
 class App extends Component {
-
-  handleLogout = event => {
-    event.preventDefault()
-    this.props.userLogout()
-  }
 
   render() {
     const { currentUser } = this.props
     return (
       <div className="App">
-          <Logo currentUser={currentUser} />
-          {currentUser ? null : <AuthForms />}
-          {currentUser ? <UsersContainer /> : null}
-          {currentUser ? <Logout handleLogout={this.handleLogout} /> : null}
+          <Logo currentUser={currentUser} /> 
+          {currentUser ? <Home currentUser={currentUser} /> : <AuthForms />}
       </div>
     )
   }
@@ -40,5 +31,5 @@ const mapState = (state) => ({
 
 export default connect(
   mapState, 
-  { getCurrentUser, userLogout }
+  { getCurrentUser }
 )(App)
