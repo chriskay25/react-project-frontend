8cbdf7a6b3effbfa01af0b33e8897283bc7d2d39
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-18 23:55:43 -0400

    Use span instead of p for easier formatting

--- src/components/GameStats.jsx
+++ src/components/GameStats.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GameStats = ({ score, gameOver }) => {
   return (
     <div className={gameOver ? 'GameStats game-over-score' : 'GameStats'}>
-      <p>Score: {score}</p>
+      <span>Score: {score}</span>
     </div>
   )
 }
