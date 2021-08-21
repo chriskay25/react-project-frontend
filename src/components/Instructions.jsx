f6e34f627633aacedecd2e9d6c2d36e1a125b0df
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-20 23:22:23 -0400

    Add more instructions on how to play

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -4,9 +4,14 @@ const Instructions = () => {
 
   return (
     <div className="Instructions">
-      <p><strong>Note: </strong>This game is best played on a desktop.</p>
-      <p>Use the arrow keys to move the green circle. Press the space bar to pause.</p> 
-      <p>Avoid oncoming enemies and stay alive. The longer you stay alive, the higher your score. Good luck!</p>
+      <div className='instructions-container'>
+        <p><strong>NOTE: </strong>This game must be played on a desktop.</p>
+        <p>Use the <span>ARROW KEYS</span> to move the green circle.</p>
+        <p>Press <span>SHIFT</span> to use bombs to clear board of enemies.</p>
+        <p>Press <span>SPACE BAR</span> to pause the game.</p> 
+        <p>Avoid oncoming enemies and stay alive. The longer you stay alive, the higher your score.</p>
+        <p>Good luck!</p>
+      </div>
     </div>
   )
 
