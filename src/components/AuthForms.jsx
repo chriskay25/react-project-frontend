484b9bd0683d0a0edce86810e22b21e254eb46e2
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-20 23:17:37 -0400

    Instead of state, useLocation hook used to hide links

--- src/components/AuthForms.jsx
+++ src/components/AuthForms.jsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import SignupForm from './SignupForm'
 import LoginForm from './LoginForm'
-import { Route, Link } from 'react-router-dom'
-
+import { Route, Link, Switch, useLocation } from 'react-router-dom'
 
 const AuthForms = () => {
-    const [clicked, setClicked] = useState(false)
-    const handleClick = () => setClicked(true)
+    const location = useLocation()
 
     return (
         <div className='auth-forms'>
-            {!clicked && <div className='form-links'>
-                <Link to='/users/new' onClick={handleClick}> Sign Up</Link>
-                <Link to='/login' onClick={handleClick}>Log In</Link>   
+            {location.pathname === '/' && <div className='form-links'>
+                <Link to='/users/new'> Sign Up</Link>
+                <Link to='/login'>Log In</Link>   
             </div>}
             
             <Route exact path="/users/new" component={SignupForm} />
