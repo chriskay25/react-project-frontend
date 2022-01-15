0e4c77e3a7c9a88ded54d8855843d734a8d256e5
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-14 23:59:37 -0500

    Add a menu into Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,57 +1,80 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { Link, useHistory } from 'react-router-dom'
-import Logout from './Logout'
-import { userLogout } from '../actions/userLogout'
-import Routes from './Routes'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import Routes from "./Routes";
 
 const Home = ({ currentUser }) => {
-    const history = useHistory()
-    const [clicked, setClicked] = useState(false)
-    const dispatch = useDispatch()
-    const [dimensions, setDimensions] = useState({
+  const dispatch = useDispatch();
+  const [menuOpen, toggle] = useState(false);
+
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  const [board, setBoard] = useState(
+    dimensions.width < 700 ? dimensions.width : 700
+  );
+
+  useEffect(() => {
+    // Use window size to set state for board size
+    function handleResize() {
+      setDimensions({
         height: window.innerHeight,
-        width: window.innerWidth
-    })
-    const [board, setBoard] = useState(dimensions.width < 700 ? dimensions.width : 700)
-    
-    useEffect(() => {
-        // Use window size to set state for board size
-        function handleResize() {
-            setDimensions({
-                height: window.innerHeight,
-                width: window.innerWidth
-            })
-        }
-        dispatch(() => {
-            setBoard(dimensions.width < 700 ? dimensions.width : 700)
-        })
-    
-        window.addEventListener('resize', handleResize)
-        return () => {
-            window.removeEventListener('resize', handleResize)
-        }
-    }, [dispatch, dimensions, board])
-
-
-    const handleLogout = () => {
-        history.push('/')
-        dispatch(userLogout)
+        width: window.innerWidth,
+      });
     }
+    dispatch(() => {
+      let smaller = Math.min(dimensions.height, dimensions.width);
+      console.log("smaller: ", smaller);
+      setBoard(smaller - 180);
+    });
 
-    return (
-        <div className='home'>
-            <div className='home-links' onClick={() => setClicked(!clicked)}>
-                <Link to='/game'>Play Game</Link>
-                <Link to='/highscores'>High Scores</Link>
-                <Link to='/instructions'>Instructions</Link> 
-            </div>
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [dispatch, dimensions, board]);
 
-            <Routes board={board} currentUser={currentUser} />
+  return (
+    <div className="home">
+      <div
+        className="menu"
+        style={{ background: menuOpen ? "#ffffff" : "#282c34" }}
+        onClick={() => toggle(!menuOpen)}
+      >
+        <span
+          style={{
+            display: "block",
+            color: menuOpen ? "#000" : "#fff",
+            padding: ".5rem 1rem",
+            cursor: "pointer",
+          }}
+        >
+          <svg
+            width="40px"
+            height="40px"
+            strokeWidth="3px"
+            stroke={menuOpen ? "#000" : "#add8e6"}
+          >
+            <path d={menuOpen ? "m 0 10, 25 20" : "m 0 10, 25 0"} />
+            <path d="m 0 18, 25 0" style={{ opacity: menuOpen ? 0 : 1 }} />
+            <path d={menuOpen ? "m 0 30, 25 -20" : "m 0 26, 25 0"} />
+          </svg>
+        </span>
+        {menuOpen && (
+          <div className="home-links">
+            <Link to="/game">Play Game</Link>
+            <Link to="/highscores">High Scores</Link>
+            <Link to="/instructions">Instructions</Link>
+            <Link to="/logout">Logout</Link>
+          </div>
+        )}
+      </div>
 
-            <Logout handleLogout={handleLogout} />
-        </div>
-    )
-}
+      <Routes board={board} currentUser={currentUser} />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
