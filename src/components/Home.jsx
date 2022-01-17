186b14c7c8f3866b7dc4cc6e7319a8eaf2bc7e65
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-17 00:00:27 -0500

    Extract Menu, simplify setting board dimensions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,76 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import Menu from "./Menu";
 import Routes from "./Routes";
 
 const Home = ({ currentUser }) => {
-  const dispatch = useDispatch();
-  const [menuOpen, toggle] = useState(false);
-
-  const [dimensions, setDimensions] = useState({
-    height: window.innerHeight,
-    width: window.innerWidth,
+  const [board, setBoard] = useState({
+    height: window.innerHeight * 0.8,
+    width: window.innerWidth * 0.9,
   });
-
-  const [board, setBoard] = useState(
-    dimensions.width < 700 ? dimensions.width : 700
-  );
 
   useEffect(() => {
     // Use window size to set state for board size
     function handleResize() {
-      setDimensions({
-        height: window.innerHeight,
-        width: window.innerWidth,
+      setBoard({
+        height: window.innerHeight * 0.8,
+        width: window.innerWidth * 0.9,
       });
     }
-    dispatch(() => {
-      let smaller = Math.min(dimensions.height, dimensions.width);
-      console.log("smaller: ", smaller);
-      setBoard(smaller - 180);
-    });
 
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [dispatch, dimensions, board]);
+  }, [board]);
 
   return (
     <div className="home">
-      <div
-        className="menu"
-        style={{ background: menuOpen ? "#ffffff" : "#282c34" }}
-        onClick={() => toggle(!menuOpen)}
-      >
-        <span
-          style={{
-            display: "block",
-            color: menuOpen ? "#000" : "#fff",
-            padding: ".5rem 1rem",
-            cursor: "pointer",
-          }}
-        >
-          <svg
-            width="40px"
-            height="40px"
-            strokeWidth="3px"
-            stroke={menuOpen ? "#000" : "#add8e6"}
-          >
-            <path d={menuOpen ? "m 0 10, 25 20" : "m 0 10, 25 0"} />
-            <path d="m 0 18, 25 0" style={{ opacity: menuOpen ? 0 : 1 }} />
-            <path d={menuOpen ? "m 0 30, 25 -20" : "m 0 26, 25 0"} />
-          </svg>
-        </span>
-        {menuOpen && (
-          <div className="home-links">
-            <Link to="/game">Play Game</Link>
-            <Link to="/highscores">High Scores</Link>
-            <Link to="/instructions">Instructions</Link>
-            <Link to="/logout">Logout</Link>
-          </div>
-        )}
-      </div>
+      {currentUser && <Menu />}
 
       <Routes board={board} currentUser={currentUser} />
     </div>
