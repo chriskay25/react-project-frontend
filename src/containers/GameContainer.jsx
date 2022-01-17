c6e8fff28ee860c2f3ffd35151c11b9b94217943
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-17 00:04:39 -0500

    Use new height and width dimensions for Board

--- src/containers/GameContainer.jsx
+++ src/containers/GameContainer.jsx
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import Board from '../components/Board';
-import Player from '../components/Player';
-import Enemy from '../components/Enemy';
-import GameStats from '../components/GameStats';
-import User from '../components/User'
-import { connect } from 'react-redux'
-import { saveGame } from '../actions/saveGame'
-import { getHighScore } from '../actions/getHighScore'
+import React, { Component } from "react";
+import Board from "../components/Board";
+import Player from "../components/Player";
+import Enemy from "../components/Enemy";
+import GameStats from "../components/GameStats";
+import User from "../components/User";
+import { connect } from "react-redux";
+import { saveGame } from "../actions/saveGame";
+import { getHighScore } from "../actions/getHighScore";
 
 class GameContainer extends Component {
-
   constructor(props) {
-    super(props)
-    const { boardSize } = this.props
-    const playerSize = (boardSize / 30)
+    super(props);
+    const { boardSize } = this.props;
+    const playerSize =
+      boardSize.width > boardSize.height
+        ? boardSize.height / 30
+        : boardSize.width / 30;
     this.state = {
       playerSize: playerSize,
       enemySize: playerSize,
@@ -28,83 +30,83 @@ class GameContainer extends Component {
       enemyID: 0,
       positions: {
         player: {
-          x: (boardSize / 2) - (playerSize / 2),
-          y: (boardSize / 2) - (playerSize / 2)
+          x: boardSize.width / 2 - playerSize / 2,
+          y: boardSize.height / 2 - playerSize / 2,
         },
-        enemies: []
+        enemies: [],
       },
       up: false,
       down: false,
       left: false,
-      right: false
-    }
+      right: false,
+    };
   }
 
   handleKeyDown = (e) => {
     switch (e.key) {
-      case 'ArrowUp': 
+      case "ArrowUp":
         this.setState({
-          up: true
-        })
+          up: true,
+        });
         break;
-      case 'ArrowDown':
+      case "ArrowDown":
         this.setState({
-          down: true
-        })
+          down: true,
+        });
         break;
-      case 'ArrowLeft':
+      case "ArrowLeft":
         this.setState({
-          left: true
-        })
+          left: true,
+        });
         break;
-      case 'ArrowRight':
+      case "ArrowRight":
         this.setState({
-          right: true
-        })
+          right: true,
+        });
         break;
-      case 'Shift':
-        this.useBomb()
+      case "Shift":
+        this.useBomb();
         break;
-      case ' ':
-        this.pauseGame()
+      case " ":
+        this.pauseGame();
         break;
       default:
         return;
     }
-  }
+  };
 
   handleKeyUp = (e) => {
     switch (e.key) {
-      case 'ArrowUp': 
+      case "ArrowUp":
         this.setState({
-          up: false
-        })
+          up: false,
+        });
         break;
-      case 'ArrowDown':
+      case "ArrowDown":
         this.setState({
-          down: false
-        })
+          down: false,
+        });
         break;
-      case 'ArrowLeft':
+      case "ArrowLeft":
         this.setState({
-          left: false
-        })
+          left: false,
+        });
         break;
-      case 'ArrowRight':
+      case "ArrowRight":
         this.setState({
-          right: false
-        })
+          right: false,
+        });
         break;
       default:
         return;
     }
-  }
+  };
 
   handlePlayerMovement = (e) => {
-    const { paused } = this.state
-    const { x, y } = this.state.positions.player
-    const { boardSize } = this.props
-    const { up, down, left, right, playerSize } = this.state
+    const { paused } = this.state;
+    const { x, y } = this.state.positions.player;
+    const { boardSize } = this.props;
+    const { up, down, left, right, playerSize } = this.state;
 
     if (up && y >= 0 && !paused) {
       this.setState({
@@ -112,22 +114,22 @@ class GameContainer extends Component {
           ...this.state.positions,
           player: {
             x,
-            y: y - 10
-          }
-        }
-      })
+            y: y - 10,
+          },
+        },
+      });
     }
 
-    if (down && (y < boardSize - playerSize) && !paused) {
+    if (down && y < boardSize.height - playerSize && !paused) {
       this.setState({
         positions: {
           ...this.state.positions,
           player: {
             x,
-            y: y + 10
-          }
-        }
-      })
+            y: y + 10,
+          },
+        },
+      });
     }
 
     if (left && x > 0 && !paused) {
@@ -136,54 +138,73 @@ class GameContainer extends Component {
           ...this.state.positions,
           player: {
             x: x - 10,
-            y
-          }
-        }
-      })
+            y,
+          },
+        },
+      });
     }
 
-    if (right && (x < (boardSize - playerSize)) && !paused) {
+    if (right && x < boardSize.width - playerSize && !paused) {
       this.setState({
         positions: {
           ...this.state.positions,
           player: {
             x: x + 10,
-            y
-          }
-        }
-      })
+            y,
+          },
+        },
+      });
     }
-
-  }
+  };
 
   randomSide = (min, max) => {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
 
   createNewEnemy = () => {
-    const { player } = this.state.positions
-    const { boardSize } = this.props
-    const { playerSize, enemyInterval, gameOver, paused } = this.state
+    const { player } = this.state.positions;
+    const { boardSize } = this.props;
+    const { playerSize, enemyInterval, gameOver, paused } = this.state;
     let newEnemy;
 
     this.setState({
-      enemyID: this.state.enemyID + 1
-    })
+      enemyID: this.state.enemyID + 1,
+    });
 
-    switch(this.randomSide(1, 4)) {
-      case 1:  // Left
-        newEnemy = { key: this.state.enemyID, x: 0, y: player.y, direction: "Left" }
+    switch (this.randomSide(1, 4)) {
+      case 1: // Left
+        newEnemy = {
+          key: this.state.enemyID,
+          x: 0,
+          y: player.y,
+          direction: "Left",
+        };
         break;
-      case 2:  // Up
-        newEnemy = { key: this.state.enemyID, x: player.x, y: 0, direction: "Up" }
+      case 2: // Up
+        newEnemy = {
+          key: this.state.enemyID,
+          x: player.x,
+          y: 0,
+          direction: "Up",
+        };
         break;
-      case 3:  // Right
-        newEnemy = { key: this.state.enemyID, x: boardSize - playerSize, y: player.y, direction: "Right" }
+      case 3: // Right
+        newEnemy = {
+          key: this.state.enemyID,
+          x: boardSize.width - playerSize,
+          y: player.y,
+          direction: "Right",
+        };
         break;
-      case 4:  // Down
-        newEnemy = { key: this.state.enemyID, x: player.x, y: boardSize - playerSize, direction: "Down" }
+      case 4: // Down
+        newEnemy = {
+          key: this.state.enemyID,
+          x: player.x,
+          y: boardSize.height - playerSize,
+          direction: "Down",
+        };
         break;
       default:
         return;
@@ -192,114 +213,128 @@ class GameContainer extends Component {
     this.setState({
       positions: {
         ...this.state.positions,
-        enemies: [...this.state.positions.enemies].concat(newEnemy)
-      }
-    })
+        enemies: [...this.state.positions.enemies].concat(newEnemy),
+      },
+    });
 
     if (!gameOver && !paused) {
-      this.enemyCreationInterval = setTimeout(this.createNewEnemy, enemyInterval)
+      this.enemyCreationInterval = setTimeout(
+        this.createNewEnemy,
+        enemyInterval
+      );
     }
-  }
+  };
 
   updateEnemyPositions = () => {
-    const { speed, playerSize, positions: {enemies} } = this.state
-    const { boardSize } = this.props
+    const {
+      speed,
+      playerSize,
+      positions: { enemies },
+    } = this.state;
+    const { boardSize } = this.props;
     this.setState({
       positions: {
         ...this.state.positions,
-        enemies: enemies.filter(enemy => !enemy.remove).map(enemy => {
-          if (enemy.y < 0 || enemy.y > boardSize - playerSize || 
-            enemy.x < 0 || enemy.x > boardSize - playerSize) {
-              enemy.remove = true
+        enemies: enemies
+          .filter((enemy) => !enemy.remove)
+          .map((enemy) => {
+            if (
+              enemy.y < 0 ||
+              enemy.y > boardSize.height - playerSize ||
+              enemy.x < 0 ||
+              enemy.x > boardSize.width - playerSize
+            ) {
+              enemy.remove = true;
               return enemy;
-          }
+            }
 
-          switch(enemy.direction) {
-            case "Left":
-              enemy.x += speed;
-              break;
-            case "Up":
-              enemy.y += speed;
-              break;
-            case "Right":
-              enemy.x -= speed;
-              break;
-            case "Down":
-              enemy.y -= speed;
-              break;
-            default:
-              break;
-          }
+            switch (enemy.direction) {
+              case "Left":
+                enemy.x += speed;
+                break;
+              case "Up":
+                enemy.y += speed;
+                break;
+              case "Right":
+                enemy.x -= speed;
+                break;
+              case "Down":
+                enemy.y -= speed;
+                break;
+              default:
+                break;
+            }
 
-          return enemy;
-        })
-      }
-    })
-  }
+            return enemy;
+          }),
+      },
+    });
+  };
 
   startGame = () => {
     this.setState({
       newHighScore: false,
       gameOver: false,
       score: 0,
-      bombs: 3
-    })
-    const { enemyInterval } = this.state
-    this.timeInterval = setInterval(this.updateTime, 1000)
-    this.scoreInterval = setInterval(this.updateScore, 100)
-    this.enemyInterval = setInterval(this.updateEnemyPositions, 50)
-    this.playerInterval = setInterval(this.handlePlayerMovement, 20)
-    this.enemyCreationInterval = setTimeout(this.createNewEnemy, enemyInterval)
-  }
+      bombs: 3,
+    });
+    const { enemyInterval } = this.state;
+    this.timeInterval = setInterval(this.updateTime, 1000);
+    this.scoreInterval = setInterval(this.updateScore, 100);
+    this.enemyInterval = setInterval(this.updateEnemyPositions, 50);
+    this.playerInterval = setInterval(this.handlePlayerMovement, 20);
+    this.enemyCreationInterval = setTimeout(this.createNewEnemy, enemyInterval);
+  };
 
   updateTime = () => {
-    this.setState((state) => ({ 
+    this.setState((state) => ({
       timeElapsed: state.timeElapsed + 1,
-    }))
-    
+    }));
+
     if (this.state.timeElapsed % 10 === 0) {
       this.setState({
         speed: this.state.speed + 1,
-        playerSize: this.state.playerSize + 2
-      })
-    } 
-    
+        playerSize: this.state.playerSize + 2,
+      });
+    }
+
     if (this.state.timeElapsed % 5 === 0) {
       this.setState({
-        enemyInterval: this.state.enemyInterval - 100
-      })
+        enemyInterval: this.state.enemyInterval - 100,
+      });
     }
-  }
+  };
 
   updateScore = () => {
     this.setState((state) => ({
-      score: state.score + 1
-    }))
-  }
+      score: state.score + 1,
+    }));
+  };
 
   useBomb = () => {
-    const { bombs } = this.state
+    const { bombs } = this.state;
     if (bombs > 0) {
       this.setState({
         positions: {
           ...this.state.positions,
-          enemies: []
-        }
-      })
+          enemies: [],
+        },
+      });
       this.setState((state) => ({
-        bombs: state.bombs - 1
-      }))
+        bombs: state.bombs - 1,
+      }));
     }
-  }
+  };
 
   pauseGame = () => {
-    const { gameOver, paused, score, speed, timeElapsed, enemyInterval } = this.state
+    const { gameOver, paused, score, speed, timeElapsed, enemyInterval } =
+      this.state;
 
-    if (gameOver) return
+    if (gameOver) return;
     if (!paused) {
-      clearInterval(this.timeInterval)
-      clearInterval(this.scoreInterval)
-      clearTimeout(this.enemyCreationInterval)
+      clearInterval(this.timeInterval);
+      clearInterval(this.scoreInterval);
+      clearTimeout(this.enemyCreationInterval);
       this.setState({
         paused: true,
         speed: 0,
@@ -307,47 +342,53 @@ class GameContainer extends Component {
         timeElapsed: 0,
         savedTime: timeElapsed,
         savedScore: score,
-        savedInterval: enemyInterval
-      })
+        savedInterval: enemyInterval,
+      });
     } else {
-      const { savedScore, savedSpeed, savedTime, savedInterval } = this.state
-      this.timeInterval = setInterval(this.updateTime, 1000)
-      this.scoreInterval = setInterval(this.updateScore, 50)
-      this.enemyCreationInterval = setTimeout(this.createNewEnemy, savedInterval)
+      const { savedScore, savedSpeed, savedTime, savedInterval } = this.state;
+      this.timeInterval = setInterval(this.updateTime, 1000);
+      this.scoreInterval = setInterval(this.updateScore, 50);
+      this.enemyCreationInterval = setTimeout(
+        this.createNewEnemy,
+        savedInterval
+      );
       this.setState({
         paused: false,
         speed: savedSpeed,
         timeElapsed: savedTime,
         score: savedScore,
-        enemyInterval: savedInterval
-      })
+        enemyInterval: savedInterval,
+      });
     }
-  }
+  };
 
   gameOver = () => {
-    const { score } = this.state
-    const { highScore } = this.props.highScore
+    const { score } = this.state;
+    const { highScore } = this.props.highScore;
     this.setState({
-      gameOver: true
-    }) 
-    this.props.saveGame({score})
+      gameOver: true,
+    });
+    this.props.saveGame({ score });
     if (score > highScore) {
       this.setState({
-        newHighScore: true
-      })
+        newHighScore: true,
+      });
     }
-    this.resetGame()
-  }
+    this.resetGame();
+  };
 
   resetGame = () => {
-    clearInterval(this.timeInterval)
-    clearInterval(this.scoreInterval)
-    clearInterval(this.enemyInterval)
-    clearInterval(this.enemyCreationInterval)
-    clearInterval(this.playerInterval)
+    clearInterval(this.timeInterval);
+    clearInterval(this.scoreInterval);
+    clearInterval(this.enemyInterval);
+    clearInterval(this.enemyCreationInterval);
+    clearInterval(this.playerInterval);
 
-    const { boardSize } = this.props
-    const playerSize = boardSize / 30
+    const { boardSize } = this.props;
+    const playerSize =
+      boardSize.width > boardSize.height
+        ? boardSize.height / 30
+        : boardSize.width / 30;
 
     this.setState({
       playerSize: playerSize,
@@ -358,23 +399,54 @@ class GameContainer extends Component {
       enemyID: 0,
       positions: {
         player: {
-          x: (boardSize / 2) - (playerSize / 2),
-          y: (boardSize / 2) - (playerSize / 2)
+          x: boardSize.width / 2 - playerSize / 2,
+          y: boardSize.height / 2 - playerSize / 2,
         },
-        enemies: []
-      }
-    })
-  }
+        enemies: [],
+      },
+    });
+  };
 
   render() {
-    const { paused, score, speed, timeElapsed, positions, enemyInterval, gameOver, playerSize, enemySize, newHighScore, bombs } = this.state
-    const { boardSize, currentUser, highScore } = this.props
+    const {
+      paused,
+      score,
+      speed,
+      timeElapsed,
+      positions,
+      enemyInterval,
+      gameOver,
+      playerSize,
+      enemySize,
+      newHighScore,
+      bombs,
+    } = this.state;
+    const { boardSize, currentUser, highScore } = this.props;
     return (
       <div className="GameContainer">
-        <Board boardSize={boardSize} playerSize={playerSize} paused={paused} gameOver={gameOver} startGame={this.startGame}>
-          <User gameOver={gameOver} currentUser={currentUser} highScore={highScore} newHighScore={newHighScore} bombs={bombs} />
-          <Player playerPosition={positions.player} playerSize={playerSize} handleKeyDown={this.handleKeyDown} handleKeyUp={this.handleKeyUp} gameOver={gameOver} />
-          {this.state.positions.enemies.map(enemy => 
+        <Board
+          boardSize={boardSize}
+          playerSize={playerSize}
+          paused={paused}
+          gameOver={gameOver}
+          startGame={this.startGame}
+        >
+          <User
+            gameOver={gameOver}
+            currentUser={currentUser}
+            score={score}
+            highScore={highScore}
+            newHighScore={newHighScore}
+            bombs={bombs}
+          />
+          <Player
+            playerPosition={positions.player}
+            playerSize={playerSize}
+            handleKeyDown={this.handleKeyDown}
+            handleKeyUp={this.handleKeyUp}
+            gameOver={gameOver}
+          />
+          {this.state.positions.enemies.map((enemy) => (
             <Enemy
               key={enemy.key}
               enemy={enemy}
@@ -382,34 +454,31 @@ class GameContainer extends Component {
               enemySize={enemySize}
               gameOver={this.gameOver}
             />
-          )}
-          <GameStats score={score} time={timeElapsed} speed={speed} enemyInterval={enemyInterval} gameOver={gameOver} />
+          ))}
         </Board>
+        {/* <GameStats time={timeElapsed} speed={speed} enemyInterval={enemyInterval} gameOver={gameOver} /> */}
       </div>
-    )
+    );
   }
 
   componentDidMount() {
-    const { currentUser } = this.props
-    this.props.getHighScore(currentUser.id)
-    this.startGame()
+    const { currentUser } = this.props;
+    this.props.getHighScore(currentUser.id);
+    this.startGame();
   }
 
   componentWillUnmount() {
-    clearInterval(this.timeInterval)
-    clearInterval(this.scoreInterval)
-    clearInterval(this.enemyInterval)
-    clearInterval(this.enemyCreationInterval)
-    clearInterval(this.playerInterval)
+    clearInterval(this.timeInterval);
+    clearInterval(this.scoreInterval);
+    clearInterval(this.enemyInterval);
+    clearInterval(this.enemyCreationInterval);
+    clearInterval(this.playerInterval);
   }
 }
 
 const mapState = (state) => ({
   currentUser: state.currentUser,
-  highScore: state.highScore
-})
+  highScore: state.highScore,
+});
 
-export default connect(
-  mapState, 
-  { saveGame, getHighScore }
-)(GameContainer)
+export default connect(mapState, { saveGame, getHighScore })(GameContainer);
