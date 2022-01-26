fd929ec9a89b718f5fba0b6808d394345dffdcb5
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-25 23:56:56 -0500

    Push home to history after successful signup

--- src/actions/addUser.js
+++ src/actions/addUser.js
@@ -1,21 +1,23 @@
-export const addUser = (data) => {
+export const addUser = (data, history) => {
   return (dispatch) => {
-    fetch('http://localhost:3000/api/v1/users', {
+    fetch("http://localhost:3000/api/v1/users", {
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
-      method: 'POST',
-      body: JSON.stringify(data)
+      method: "POST",
+      body: JSON.stringify(data),
     })
-    .then(response => response.json())
-    .then(userData => {
-      if (userData.error) {
-        alert(userData.error)
-      } else {
-        localStorage.setItem('token', userData.jwt)
-        dispatch({type: 'ADD_USER', payload: userData.user})
-      }
-    })
-  }
-}
+      .then((response) => response.json())
+      .then((userData) => {
+        console.log("signupdata: ", userData);
+        if (userData.error) {
+          alert(userData.error);
+        } else {
+          localStorage.setItem("token", userData.jwt);
+          history.push("/home");
+          dispatch({ type: "ADD_USER", payload: userData.user });
+        }
+      });
+  };
+};
