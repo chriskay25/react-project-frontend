f4ee311822e85dd7a2fdb55ba11728837226a854
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-22 19:51:04 -0400

    Auto-edits from Prettier, use gamestate to set text color

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-const User = ({ currentUser, gameOver, score, highScore, newHighScore, bombs }) => {
+const User = ({
+  currentUser,
+  gameOver,
+  paused,
+  score,
+  highScore,
+  newHighScore,
+  bombs,
+}) => {
+  const [color, setColor] = useState(null);
+
+  useEffect(() => {
+    const gamestate = () => {
+      gameOver && setColor("#cee1e2");
+      paused && setColor("#000");
+      !gameOver && !paused && setColor("#000");
+    };
+
+    gamestate();
+  }, [paused, gameOver]);
+
   return (
     <>
-      <div className={gameOver ? 'User game-over-user-info' : 'User'}>
-        <p><strong>Player:</strong> {currentUser.username}</p>
-        <p><strong>High Score:</strong> {highScore ? highScore : currentUser.highScore}</p>
+      <div className={gameOver ? "User game-over-user-info" : "User"}>
+        <p style={{ fontWeight: "800", color: color }}>
+          Player: {currentUser.username}
+        </p>
+        <p style={{ fontWeight: "800", color: color }}>
+          High Score: {highScore ? highScore : currentUser.highScore}
+        </p>
       </div>
-      <div className='new-highscore'>
-        {newHighScore ? <h1>New High Score!</h1> : ''}
+      <div className="new-highscore">
+        {newHighScore ? <h1>New High Score!</h1> : ""}
       </div>
-      <span className='bombs'><strong>Bombs:</strong> {bombs}</span>
-      <div className={gameOver ? 'score game-over-score' : 'score'}>
+      <span style={{ fontWeight: "800", color: color }} className="bombs">
+        Bombs: {bombs}
+      </span>
+      <div
+        className={gameOver ? "score game-over-score" : "score"}
+        style={{ color: color }}
+      >
         <span>Score: {score}</span>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default User
+export default User;
