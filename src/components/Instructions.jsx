d08f6b439e5ea88df865c67a422990c7bb45b5e3
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-22 19:45:22 -0400

    Use HTML tags to add structure

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React from "react";
 
 const Instructions = () => {
-
   return (
     <div className="Instructions">
-      <div className='instructions-container'>
-        <p><strong>NOTE: </strong>This game must be played on a desktop.</p>
-        <p>Use the <span>ARROW KEYS</span> to move the green circle.</p>
-        <p>Press <span>SHIFT</span> to use bombs to clear board of enemies.</p>
-        <p>Press <span>SPACE BAR</span> to pause the game.</p> 
-        <p>Avoid oncoming enemies and stay alive. The longer you stay alive, the higher your score.</p>
-        <p>Good luck!</p>
+      <div className="instructions-container">
+        <p
+          style={{ fontSize: "11px", textAlign: "center", color: "lightblue" }}
+        >
+          <strong>NOTE: </strong>For now, this game must be played on a desktop.
+        </p>
+        <p>
+          Avoid oncoming enemies and stay alive. The longer you stay alive, the
+          higher your score. Enemies get faster and spawn more frequently as the
+          game progresses, and the player's circle also grows over time.
+        </p>
+        <h2 style={{ textAlign: "center", marginTop: "3rem", fontWeight: 800 }}>
+          CONTROLS
+        </h2>
+        <ul className="instructions-controls">
+          <li>
+            <span style={{ textAlign: "right" }}>Move Player: </span>
+            <span className="keyboard-key">ARROW KEYS</span>
+          </li>
+          <li>
+            <span style={{ textAlign: "right" }}>Use Bomb: </span>
+            <span className="keyboard-key">SHIFT</span>
+          </li>
+          <li>
+            <span style={{ textAlign: "right" }}>Pause: </span>
+            <span className="keyboard-key">SPACEBAR</span>
+          </li>
+        </ul>
+        <p
+          style={{
+            textAlign: "center",
+            marginTop: "4rem",
+            fontSize: "3.5rem",
+            fontWeight: 400,
+            color: "lightblue",
+          }}
+        >
+          Good luck!
+        </p>
       </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default Instructions
+export default Instructions;
