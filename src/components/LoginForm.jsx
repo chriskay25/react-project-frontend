4540c1738164a7f35e56a23dc5356a5ccc758cef
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-22 19:49:02 -0400

    Auto-edits from Prettier, add class to submit bttns

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,51 +1,67 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { userLogin } from '../actions/userLogin';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { userLogin } from "../actions/userLogin";
+import { Link } from "react-router-dom";
 
 class LoginForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      username: '',
-      password: ''
-    }
+      username: "",
+      password: "",
+    };
   }
 
   handleChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
   handleSubmit = (event) => {
-    event.preventDefault()
-    const { history } = this.props
-    this.props.userLogin(this.state, history)
+    event.preventDefault();
+    const { history } = this.props;
+    this.props.userLogin(this.state, history);
     this.setState({
-      username: '',
-      password: '',
-    })
-  }
+      username: "",
+      password: "",
+    });
+  };
 
   render() {
-    const { username, password } = this.state
+    const { username, password } = this.state;
     return (
-      <div className="FormBox" >
-        <h4 style={{padding: '1em'}}>LOGIN</h4>
+      <div className="FormBox">
         <form className="InputForm" onSubmit={this.handleSubmit}>
-          <label htmlFor="username">Username: </label>
-          <input type="text" placeholder="Username" name="username" value={username} onChange={this.handleChange} />
-          <br />
-          <label htmlFor="password">Password: </label>
-          <input type="password" placeholder="Password" name="password" value={password} onChange={this.handleChange} />
-          <br />
-          <input type="submit" value="Login" />
+          <h2 style={{ padding: "1rem" }}>LOGIN</h2>
+          <div className="form-inputs">
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+            />
+            <br />
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <input className="auth-submit" type="submit" value="Submit" />
         </form>
-        <Link to='/users/new' className='form-switch'> Need to sign up?</Link>
+        <Link to="/auth/signup" className="form-switch">
+          {" "}
+          Need to sign up?
+        </Link>
       </div>
-    )
+    );
   }
 }
 
-export default connect(null, { userLogin })(LoginForm)
+export default connect(null, { userLogin })(LoginForm);
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,49 +1,67 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { addUser } from '../actions/addUser';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addUser } from "../actions/addUser";
+import { Link } from "react-router-dom";
 
 class SignupForm extends Component {
-
-  state = {
-    username: '',
-    password: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
   }
 
   handleChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
   handleSubmit = (event) => {
-    event.preventDefault()
-    const { username, password } = this.state
-    this.props.addUser({ username, password })
+    event.preventDefault();
+    const { history } = this.props;
+    this.props.addUser(this.state, history);
     this.setState({
-      username: '',
-      password: '',
-    })
-  }
+      username: "",
+      password: "",
+    });
+  };
 
   render() {
-    const { username, password } = this.state
+    const { username, password } = this.state;
     return (
-      <div className="FormBox" >
-        <h4 style={{padding: '1em'}}>NEW USER</h4>
+      <div className="FormBox">
         <form className="InputForm" onSubmit={this.handleSubmit}>
-          <label htmlFor="username">Username: </label>
-          <input type="text" placeholder="Username" name="username" value={username} onChange={this.handleChange} />
-          <br />
-          <label htmlFor="password">Password: </label>
-          <input type="password" placeholder="Password" name="password" value={password} onChange={this.handleChange} />
-          <br />
-          <input type="submit" />
+          <h2 style={{ padding: "1rem" }}>SIGN UP</h2>
+          <div className="form-inputs">
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+            />
+            <br />
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+            <br />
+          </div>
+          <input className="auth-submit" type="submit" />
         </form>
-        <Link to='/login' className='form-switch'>Already have an account?</Link> 
+        <Link to="/auth/login" className="form-switch">
+          Already have an account?
+        </Link>
       </div>
-    )
+    );
   }
 }
 
-export default connect(null, { addUser })(SignupForm)
+export default connect(null, { addUser })(SignupForm);
