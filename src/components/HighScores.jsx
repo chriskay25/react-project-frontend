970846ae13e80ec3c3cb7a29a18ca1c022a5429f
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-03-22 20:01:24 -0400

    Remove link to start new game

--- src/components/HighScores.jsx
+++ src/components/HighScores.jsx
@@ -1,49 +1,46 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { getHighScores } from '../actions/getHighScores'
-import { getCurrentUser } from '../actions/getCurrentUser'
-import HighScore from './HighScore'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getHighScores } from "../actions/getHighScores";
+import { getCurrentUser } from "../actions/getCurrentUser";
+import HighScore from "./HighScore";
 
 const HighScores = () => {
-  const dispatch = useDispatch()
-  const currentUser = useSelector(state => state.currentUser)
-  const games = useSelector(state => state.highScores)
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.currentUser);
+  const games = useSelector((state) => state.highScores);
 
   useEffect(() => {
-    dispatch(getCurrentUser())
-    dispatch(getHighScores())
-  }, [dispatch])
+    dispatch(getCurrentUser());
+    dispatch(getHighScores());
+  }, [dispatch]);
 
   const showHighScores = () => {
     return games.map((game) => {
-      return <HighScore key={game.id} game={game} />
-    })
-  }
+      return <HighScore key={game.id} game={game} />;
+    });
+  };
 
   const showUsersHighScores = () => {
     return currentUser.highScores.map((game) => {
-      return <HighScore key={game.id} usersScore={game} />
-    })
-  }
+      return <HighScore key={game.id} usersScore={game} />;
+    });
+  };
 
   return (
-    <div className='highscores-container'>
-      <Link to='/game'>Play Game</Link>
-      <h1 style={{textAlign: 'center', fontSize: '4rem'}}>HIGH SCORES</h1>
+    <div className="highscores-container">
+      <h1 style={{ textAlign: "center", fontSize: "4rem" }}>HIGH SCORES</h1>
       <div className="highscores">
-        <div className='highscores-col'>
+        <div className="highscores-col">
           <h2>All Time</h2>
           {showHighScores()}
         </div>
-        <div className='highscores-col'>
-          <h2>{currentUser.username + "'s"}</h2>
+        <div className="highscores-col">
+          <h2>{currentUser.username}</h2>
           {showUsersHighScores()}
         </div>
       </div>
     </div>
-  ) 
-  
-}
+  );
+};
 
-export default HighScores
+export default HighScores;
